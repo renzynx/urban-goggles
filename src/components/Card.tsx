e81b956ffde5bb969793bf2de9b07869f5ba3cbc
renzynx <69468436+renzynx@users.noreply.github.com>
2024-01-07 17:49:34 +0000

    fix: ui error

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -20,32 +20,22 @@ const Card: FC<CardProps> = ({ comic }) => {
   }
 
   return (
-    <a className="relative block aspect-auto h-full w-full cursor-pointer">
-      <div className="group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-blue-600 dark:bg-black relative border-neutral-200 dark:border-neutral-800">
-        <Image
-          className="relative object-center transition duration-300 ease-in-out group-hover:scale-105"
-          src={comic.thumbnail}
-          alt={comic.title!}
-          width={250}
-          height={70}
-        />
-      </div>
-      <div className="absolute top-0 left-0 flex w-full pt-4 @container/label px-2 cursor-default">
-        <div className="flex items-center rounded-full border bg-white/70 p-1 text-black backdrop-blur-md dark:border-neutral-800 dark:bg-black/70 dark:text-white">
-          <h3 className="flex-grow pl-2 leading-none tracking-tight px-3 py-2">
-            {comic.title}
-          </h3>
-        </div>
-      </div>
+    <div className="relative w-[200px] h-[300px] group cursor-pointer">
+      <Image
+        src={comic.thumbnail || "/images/placeholder.png"}
+        alt={comic.title}
+        layout="fill"
+        objectFit="cover"
+        className="opacity-80 rounded-md group-hover:opacity-50 transition-opacity duration-300"
+      />
+      <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-background to-transparent rounded-b-md p-2 shadow-lg">
+        <h1 className="text-white font-bold">{comic.title}</h1>
+        <p className="text-white text-sm">
+          {comic.chapters.length} Chapter{comic.chapters.length > 1 && "s"}
+        </p>
 
-      <div className="absolute bottom-0 left-0 flex w-full px-4 pb-4 @container/label lg:px-20 ">
-        <div className="flex items-center rounded-full p-1 text-xs font-semibold text-black dark:text-white">
-          <p className="flex-none rounded-full bg-blue-600 p-2 text-white">
-            Chapter {chapter_no}
-          </p>
-        </div>
       </div>
-    </a>
+    </div>
   );
 };
 
